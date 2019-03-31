c596abc3c4483f23bfaf2ba0a176b06e621ef893
ajayraj <ajayraj98@gmail.com>
2019-03-31 00:01:16 -0700

    added the train table in html

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,11 +3,33 @@ $(document).ready(function () {
     function buildhtml(){
         $("body").append($("<div>").addClass("container"));
         addJumboTron();
+        addTrainTable();
     }
     function addJumboTron(){
         $(".container").append($("<div>").addClass("jumbotron").attr("id", "myJumbotron"));
         $("#myJumbotron").append($("<h1>").addClass("text-center").html("Ajay's Trains"))
         $("#myJumbotron").append($("<h3>").addClass("text-center").html("Train Schecule"))
+    }
+
+    function addTrainTable(){
+        $(".container").append($("<div>").addClass("row").attr("id", "firstRow"));
+        $("#firstRow").append($("<div>").addClass("col-lg-12").attr("id", "firstColumn"));
+        $("#firstColumn").append($("<div>").addClass("card").attr("id","firstCard"));
+        $("#firstCard").append($("<div>").addClass("panel-heading").attr("id","table-heading"));
+        $("#table-heading").append($("<h3>").addClass("panel-title").attr("id","panelTitle").html("<strong>Current Train Schedule</strong>"));
+
+        $("firstCard").append($("<div>").addClass("panel-body").attr("id","panelBody"));
+        $("panelBody").append($("<table>").addClass("table table-hober").attr("id","train-table"));
+        $("train-table").append($("<thead>").attr("id", "firstThead"));
+        $("#firstThead").append($("<tr>").attr("id","firstTR"));
+        $("firstTR").append($("<th>").html("Train Name"));
+        $("firstTR").append($("<th>").html("Destination"));
+        $("firstTR").append($("<th>").html("Frequency (Min)"));
+        $("firstTR").append($("<th>").html("Next Arrival"));
+        $("firstTR").append($("<th>").html("Minutes Away"));
+
+        $("#train-table").append($("<tbody>"));
+
     }
 
 });
